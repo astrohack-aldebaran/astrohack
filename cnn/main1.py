8d589576ac66819330b9dc8f00a04a40b4d38b47
lameeus <Laurens.Meeus@UGent.be>
2017-04-29 21:45:28 +0200

    cnn + fc also reading the depth

--- cnn/main1.py
+++ cnn/main1.py
@@ -16,13 +16,13 @@ os.chdir('/Users/Laurens_240/Documents/GitHub/astrohack/')
 class Flag(object):
     """ Contains all settings """
     image_width = 64
-    lr = 1.0e-5
+    lr = 1.0e-6
     bool_load = True
-    iters = 1000
+    iters = 100
 
-    layer_size = [[64, 64], [30, 30], [12, 12], [4, 4], [1, 1]]
-    kernel_size = [[5, 5], [5, 5], [5, 5], [4, 4]]
-    kernel_depth = [1, 10, 10, 10, 10, 1]
+    layer_size = [[64, 64], [30, 30], [13, 13], [5, 5], [1, 1]]
+    kernel_size = [[5, 5], [5, 5], [4, 4], [5, 5]]
+    kernel_depth = [1, 10, 10, 10, 1]
     layer_types = ['convstride', 'convstride', 'convstride', 'conv']
 
 
@@ -36,7 +36,16 @@ def data_i(i_start, i_end):
         name_y = folder + 'info_{}.csv'.format(im_i)
 
         x.append(open_csv(name_x))
-        y.append(open_csv(name_y))
+
+        y_i = open_csv(name_y)
+
+        y.append(y_i)
+
+
+    #
+    #
+    #
+    # 1/0
 
     x = np.asarray(x)
     y = np.asarray(y)
@@ -57,6 +66,19 @@ def test_data():
     return data_i(40, n_images)
 
 
+def leaky_relu(x, alpha=0., max_value=None):
+    '''ReLU.
+
+    alpha: slope of negative section.
+    '''
+    negative_part = tf.nn.relu(-x)
+    x = tf.nn.relu(x)
+    if max_value is not None:
+        x = tf.clip_by_value(x, 0., max_value)
+    x -= tf.constant(alpha) * negative_part
+    return x
+
+
 class Network(object):
     def __init__(self, flag):
         self.flag = flag
@@ -65,10 +87,12 @@ class Network(object):
 
         n_layers = len(self.flag.layer_types)
 
+        # DON't scale to log, background is not 0
+        # in_pointer = tf.log(tf.maximum(X, 1.0e-10))
         in_pointer = X
         out_pointer = None
 
-        params = {}
+        self.params  = {}
 
         for ind_layer in range(n_layers):
 
@@ -78,25 +102,42 @@ class Network(object):
             kernel_depth_in = self.flag.kernel_depth[ind_layer]
             kernel_depth_out = self.flag.kernel_depth[ind_layer + 1]
 
-            W = tf.Variable(tf.zeros([kernel_size[0], kernel_size[1], kernel_depth_in, kernel_depth_out]),
+            #weight init
+            shape = [kernel_size[0], kernel_size[1], kernel_depth_in, kernel_depth_out]
+            stddev = np.sqrt(2 / (shape[0] * shape[1] * shape[2] +
+                                  shape[0] * shape[1] * shape[3]))
+            initial = tf.truncated_normal(shape, stddev=stddev)
+            W = tf.Variable(initial,
                             name='W_{}'.format(ind_layer))
-            b = tf.Variable(tf.zeros([1, 1, 1, kernel_depth_out]),
-                            name='b_{}'.format(ind_layer))
 
-            params.update({W.name : W, b.name : b})
+            shape = [1, 1, 1, kernel_depth_out]
+            stddev = 0
+            initial = tf.truncated_normal(shape, stddev = stddev)
+            b = tf.Variable(initial, name='b_{}'.format(ind_layer))
+
+            self.params.update({W.name : W, b.name : b})
+
+            def conv_layer(strides):
+                z_i = tf.nn.conv2d(in_pointer, W, strides=strides, padding='VALID') + b
+                # y_i = tf.nn.relu(z_i)
+                #todo leaky relu
+                y_i = leaky_relu(z_i, alpha=0.001)
+                return y_i
 
             if layer_type == 'conv':
-
-                out_pointer = tf.nn.conv2d(in_pointer, W, strides= [1, 1, 1, 1], padding = 'VALID') + b
+                strides = [1, 1, 1, 1]
+                out_pointer = conv_layer(strides)
                 in_pointer = out_pointer
             elif layer_type == 'convstride':
-
-                out_pointer = tf.nn.conv2d(in_pointer, W, strides= [1, 2, 2, 1], padding = 'VALID') + b
+                strides = [1, 2, 2, 1]
+                out_pointer = conv_layer(strides)
                 in_pointer = out_pointer
 
             else:
                 #todo
                 1/0
+
+
 
         # # flatten the images into a single line of pixels
         # # -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
@@ -116,7 +157,7 @@ class Network(object):
 
         self.y_guess = out_pointer
 
-        self.saver = tf.train.Saver(params)
+        self.saver = tf.train.Saver(self.params)
 
     def get_Y(self):
         return self.y_guess
@@ -127,28 +168,59 @@ def main():
     #
     # · · · · · · · · · ·       (input data, flattened pixels)       X [batch, 784]        # 784 = 28 * 28
     # \x/x\x/x\x/x\x/x\x/    -- fully connected layer (softmax)      W [784, 10]     b[10]
-    #   · · · · · · · ·                                              Y [batch, 10]
+    #   · · · · · · · ·                                              M_guess [batch, 10]
 
     # The model is:
     #
-    # Y = softmax( X * W + b)
+    # M_guess = softmax( X * W + b)
     #              X: matrix for 100 grayscale images of 28x28 pixels, flattened (there are 100 images in a mini-batch)
     #              W: weight matrix with 784 lines and 10 columns
     #              b: bias vector with 10 dimensions
     #              +: add with broadcasting: adds the vector to each line of the matrix (numpy)
     #              softmax(matrix) applies softmax on each line
     #              softmax(line) applies an exp to each value then divides by the norm of the resulting line
-    #              Y: output matrix with 100 lines and 10 columns
+    #              M_guess: output matrix with 100 lines and 10 columns
 
     flag = Flag()
 
     # input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
     X = tf.placeholder(tf.float32, [None, flag.image_width, flag.image_width, 1], name = 'X')
+    d_place = tf.placeholder(tf.float32, [None, 1, 1, 1], name = 'd')
 
     net = Network(flag)
     net.build_Y(X)
 
     Y = net.get_Y()
+
+    x_fc = tf.concat([Y, tf.pow(d_place/100, 3.0)], axis= 3)
+
+    def foo():
+        # weight init
+        shape = [1, 1, 2, 1]
+        stddev = np.sqrt(2 / (shape[0] * shape[1] * shape[2] +
+                              shape[0] * shape[1] * shape[3]))
+        initial = tf.truncated_normal(shape, stddev=stddev)
+        W = tf.Variable(initial,
+                        name='W_extra')
+
+        shape = [1, 1, 1, 1]
+        stddev = 0
+        initial = tf.truncated_normal(shape, stddev=stddev)
+        b = tf.Variable(initial, name='b_extra')
+
+
+        # todo update params
+        net.params.update({W.name: W, b.name: b})
+
+        strides = [1,1,1,1]
+        z_i = tf.nn.conv2d(x_fc, W, strides=strides, padding='VALID') + b
+
+        return z_i
+
+
+
+    M_guess = foo()
+    net.saver = tf.train.Saver(net.params)
 
     # correct answers will go here
     Y_ = tf.placeholder(tf.float32, [None, 1, 1, 1], name = 'Y_truth')
@@ -157,24 +229,30 @@ def main():
 
 
     # loss function: cross-entropy = - sum( Y_i * log(Yi) )
-    #                           Y: the computed output vector
+    #                           M_guess: the computed output vector
     #                           Y_: the desired output vector
 
     ln10 = 2.30258509299
 
-    # cost = tf.reduce_mean(tf.divide(tf.squared_difference(tf.pow(10.0, Y), tf.pow(10.0, Y_), name = 'squar_diff'),
+    # cost = tf.reduce_mean(tf.divide(tf.squared_difference(tf.pow(10.0, M_guess), tf.pow(10.0, Y_), name = 'squar_diff'),
     #                                 tf.square( tf.pow(10.0, Y_)*ln10*errM_)), name= 'cost')
 
     # todo switch between
-    # cost = tf.reduce_mean(tf.divide(tf.squared_difference(1.0, tf.pow(10.0, Y_ - Y)),
+    # cost = tf.reduce_mean(tf.divide(tf.squared_difference(1.0, tf.pow(10.0, Y_ - M_guess)),
     #                                 tf.square(ln10 * errM_)), name='cost')
     # todo remove 11!
-    cost = tf.reduce_mean(tf.divide(tf.squared_difference(1.0, tf.pow(10.0, Y_ - Y - 11.0)),
-                                                                    tf.square(ln10 * errM_)), name='cost')
+    # cost = tf.reduce_mean(tf.divide(tf.squared_difference(1.0, tf.pow(10.0, Y_ - M_guess)),
+    #                                                                 tf.square(ln10 * errM_)), name='cost')
+    # cost = tf.log(cost)
+
+    # cost =  tf.reduce_mean(tf.squared_difference(Y_,M_guess))
+    cost = tf.reduce_mean(tf.squared_difference(Y_, M_guess))
+
+
 
 
     # # accuracy of the trained model, between 0 (worst) and 1 (best)
-    # correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+    # correct_prediction = tf.equal(tf.argmax(M_guess, 1), tf.argmax(Y_, 1))
     # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     # training, learning rate = 0.005
@@ -184,8 +262,8 @@ def main():
     # matplotlib visualisation
     # allweights = tf.reshape(W, [-1])
     # allbiases = tf.reshape(b, [-1])
-    # I = tensorflowvisu.tf_format_mnist_images(X, Y, Y_)  # assembles 10x10 images by default
-    # It = tensorflowvisu.tf_format_mnist_images(X, Y, Y_, 1000, lines=25)  # 1000 images on 25 lines
+    # I = tensorflowvisu.tf_format_mnist_images(X, M_guess, Y_)  # assembles 10x10 images by default
+    # It = tensorflowvisu.tf_format_mnist_images(X, M_guess, Y_, 1000, lines=25)  # 1000 images on 25 lines
     # datavis = tensorflowvisu.MnistDataVis()
 
     # init
@@ -201,9 +279,9 @@ def main():
     c_t = []
     c_t_test = []
 
-    def info_foo(x_in, y_in, errM):
+    def info_foo(x_in, y_in, errM, d):
 
-        [c, y] = sess.run([cost, Y], feed_dict={X: x_in, Y_: y_in, errM_: errM})
+        [c, y] = sess.run([cost, M_guess], feed_dict={X: x_in, Y_: y_in, errM_: errM, d_place: d})
 
         y_mean_guess = np.mean(y)
         y_mean = np.mean(y_in)
@@ -226,7 +304,9 @@ def main():
 
             errM = batch_Y[:, :, :, 2:3]
 
-            c = info_foo(batch_X, batch_Y_mass, errM)
+            d= batch_Y[:, :, :, 3:4]
+
+            c = info_foo(batch_X, batch_Y_mass, errM, d)
             c_t.append(c)
 
         if update_test_data:
@@ -235,12 +315,14 @@ def main():
             batch_Y_mass_te = batch_Y_te[:, :, :, 1:2]
             errM_te = batch_Y_te[:, :, :, 2:3]
 
-            c = info_foo(batch_X_te, batch_Y_mass_te, errM_te)
+            d_te = batch_Y_te[:, :, :, 3:4]
+
+            c = info_foo(batch_X_te, batch_Y_mass_te, errM_te, d_te)
 
             c_t_test.append(c)
 
         # the backpropagation training step
-        sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y_mass, errM_: errM})
+        sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y_mass, errM_: errM, d_place: d})
 
 
 
